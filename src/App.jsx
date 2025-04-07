95544d522448271ccc850931a341dab2ca37d4fb
Kelly Ko <kellyko2003@gmail.com>
2025-04-07 13:49:58 -0600

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,50 @@ import "./App.css";
 
 function App() {
   const comics = [
-    "../images/Handwashing dogtor 1.png",
-    "../images/Handwashing dogtor 2.png",
-    "../images/Handwashing dogtor 3.png",
-    "../images/Vaccine dogtor 1.png",
-    "../images/Vaccine dogtor 2.png",
-    "../images/Masks dogtor 1.png",
-    "../images/Masks dogtor 2.png",
+    {
+      title: "Hand Washing",
+      images: [
+        "../images/Handwashing dogtor 1.png",
+        "../images/Handwashing dogtor 2.png",
+        "../images/Handwashing dogtor 3.png",
+      ],
+    },
+    {
+      title: "Vaccines",
+      images: [
+        "../images/Vaccine dogtor 1.png",
+        "../images/Vaccine dogtor 2.png",
+      ],
+    },
+    {
+      title: "Masks",
+      images: ["../images/Masks dogtor 1.png", "../images/Masks dogtor 2.png"],
+    },
+    {
+      title: "Sunscreen",
+      images: ["../images/comic_1.jpg"],
+    },
   ];
   return (
     <>
       <div class="main">
-        <h1 id="title">Dogtor Comics</h1>
-        <p id="byline">by Steven Ko</p>
-        {comics.map((path) => {
-          return <img className="comic_img" src={path}></img>;
+        <div id="header">
+          <img id="header-img" src="./favicon.ico"></img>
+          <section>
+            <h1 id="title">Dogtor Comics</h1>
+            <p id="byline">by Steven Ko</p>
+          </section>
+        </div>
+        {comics.map((comic) => {
+          return (
+            <>
+              <p id="comic-title">{comic.title}</p>
+              {comic.images.map((path) => {
+                return <img className="comic_img" src={path}></img>;
+              })}
+              <hr id="comic-separator" />
+            </>
+          );
         })}
       </div>
     </>
